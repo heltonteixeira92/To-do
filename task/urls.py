464904ec2a928923e707b126d66ec8a6511c2662
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2021-01-14 13:38:23 -0300

    Delete

--- task/urls.py
+++ task/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('update_task/<str:pk>/', views.updateTask, name="update_task")
+    path('update_task/<str:pk>/', views.updateTask, name="update_task"),
+    path('delete/<str:pk>/', views.deleteTask, name="delete")
 
 ]
--- task/views.py
+++ task/views.py
@@ -24,4 +24,25 @@ def index(request):
 
 def updateTask(request, pk):
     task = Task.objects.get(id=pk)
-    return render(request, 'tasks/update_task.html')
+
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+
+    context = {'form': form}
+
+    return render(request, 'tasks/update_task.html', context)
+
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context = {'item': item}
+    return render(request, 'tasks/delete.html', context)
